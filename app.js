b751ce44c9f5692bc5dc42d2ffbc9ad92ce23620
Roger <winter30009@yahoo.com.tw>
2022-05-25 13:24:37 +0800

    set up login route and main alert div

--- app.js
+++ app.js
@@ -13,6 +13,17 @@ app.get('/', (req, res) => {
   res.render('index')
 })
 
+app.post('/login', (req, res) => {
+  const { email, password } = req.body
+  const user = users.find(user =>
+    user.email === email && user.password === password
+  )
+  if (!user) {
+    res.send('You have a wrong email or password')
+  }
+  res.render('login', { user })
+})
+
 app.listen(port, () => {
   console.log(`App is now running on http://localhost:${port}`)
 })
